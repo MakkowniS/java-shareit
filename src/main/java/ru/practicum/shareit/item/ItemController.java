eb3cc3c932357636d1effc3855317210874d2f05
MakkowniS <makkownis@yandex.ru>
2026-02-04 18:04:17 +1000

    Повторный тест

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -25,20 +25,17 @@ public class ItemController {
     }
 
     @GetMapping("/search")
-    public List<ItemDto> searchItemToRent(@RequestParam String text){
+    public List<ItemDto> searchItemToRent(@RequestParam String text) {
         return itemService.searchItemToRent(text);
     }
 
     @PostMapping
-    public ItemDto saveItem(@Valid @RequestBody ItemDto item,
-                            @RequestHeader("X-Sharer-User-Id") Long userId){
+    public ItemDto saveItem(@Valid @RequestBody ItemDto item, @RequestHeader("X-Sharer-User-Id") Long userId) {
         return itemService.saveItem(item, userId);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto editItem(@RequestBody ItemDto item,
-                            @PathVariable Long itemId,
-                            @RequestHeader("X-Sharer-User-Id") Long userId){
+    public ItemDto editItem(@RequestBody ItemDto item, @PathVariable Long itemId, @RequestHeader("X-Sharer-User-Id") Long userId) {
         return itemService.editItem(item, itemId, userId);
     }
 }
