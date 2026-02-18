e3cf0bc20a3436350fac102df21f8e53f60d1e57
MakkowniS <makkownis@yandex.ru>
2026-02-18 17:04:28 +1000

    Реализовать метод getBookingById

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -1,13 +1,19 @@
 package ru.practicum.shareit.booking;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.practicum.shareit.booking.dto.BookingDtoResponse;
+import ru.practicum.shareit.error.NotFoundException;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BookingServiceImpl implements BookingService {
+
+    private final BookingRepository bookingRepository;
+
     @Override
     public List<BookingDtoResponse> getBookingsByState(String state, Long userId) {
         return List.of();
@@ -15,7 +21,9 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public BookingDtoResponse getBookingById(Long bookingId) {
-        return null;
+        return bookingRepository.findById(bookingId)
+                .map(BookingMapper::mapToBookingDtoResponse)
+                .orElseThrow(() -> new NotFoundException("Вещь не найдена"));
     }
 
     @Override
