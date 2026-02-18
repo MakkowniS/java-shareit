f98af0e7c451b0d79e7fe1b93851a4a9f844b23d
MakkowniS <makkownis@yandex.ru>
2026-02-18 14:42:37 +1000

    Подключение БД Настройка схемы БД Расстановка аннотаций JPA

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,30 @@
 package ru.practicum.shareit.booking;
 
+import jakarta.persistence.*;
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.Instant;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Entity
+@Table(name = "bookings")
+@Getter @Setter @ToString @EqualsAndHashCode(of = "id")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
+
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private BookingStatus state;
+
+    @Column(name = "creation_date", nullable = false)
+    private Instant createdAt = Instant.now();
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,12 +1,19 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
-@Builder
+@Entity
+@Table(name = "users")
+@Getter @Setter @ToString @EqualsAndHashCode(of = "id")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false)
     String name;
+
+    @Column(nullable = false, unique = true)
     String email;
 }
