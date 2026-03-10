1c1bc80e7e12723b7cf4df3a171fb08aa36f4cc8
MakkowniS <makkownis@yandex.ru>
2026-03-10 14:00:46 +1000

    Добавление логов

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.user;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDtoRequest;
@@ -10,33 +11,39 @@ import ru.practicum.shareit.user.dto.UserDtoRequest;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserClient userClient;
 
     @GetMapping
     public ResponseEntity<Object> getUsers() {
+        log.info("Получение пользователей.");
         return userClient.getUsers();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<Object> getUserById(@Positive @PathVariable Long userId) {
+        log.info("Получение пользователя с ID={}", userId);
         return userClient.getUserById(userId);
     }
 
     @PostMapping
     public ResponseEntity<Object> createUser(@Valid @RequestBody UserDtoRequest userDtoRequest) {
+        log.info("Создание пользователя {}", userDtoRequest);
         return userClient.createUser(userDtoRequest);
     }
 
     @PatchMapping("/{userId}")
     public ResponseEntity<Object> updateUser(@Positive @PathVariable Long userId,
                                              @RequestBody UserDtoRequest userDtoRequest) {
+        log.info("Изменение пользователя с ID={}, {}", userId, userDtoRequest);
         return userClient.updateUser(userId, userDtoRequest);
     }
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUser(@Positive @PathVariable Long userId) {
+        log.info("Удаление пользователя с ID={}", userId);
         return userClient.deleteUser(userId);
     }
 
